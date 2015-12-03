0aa977d4e014e20b356e3f336cfdf17e4c646a2a
Sydney Selch <sydney_selch@Sydneys-MacBook-Pro.local>
2015-12-03 15:38:28 -0500

    Javascript added but not linked correctly

---
+++ _includes/js/colour.js
@@ -0,0 +1,15 @@
+var $btn = $('.colour-btn');
+var $pink = $('.pink');
+var $green = $('.green');
+var $blue = $('.blue');
+var $maroon = $('.maroon');
+var $gray = $('.gray');
+
+$btn.on('click', function () {
+    $pink.toggleClass({
+        '.green'
+        '.maroon'
+        '.blue'
+        '.gray'
+    });
+});
